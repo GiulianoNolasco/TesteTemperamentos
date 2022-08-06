ce770a534e354235d579229c3141b66abecddc42
Giuliano Nolasco <110136470+GiulianoNolasco@users.noreply.github.com>
2022-08-05 21:54:50 -0300

    Commit Inicial

---
+++ QuestionarioTemperamentos/src/main/java/com/mycompany/questionariotemperamentos/QuestionarioTemperamentos.java
@@ -0,0 +1,9 @@
+package com.mycompany.questionariotemperamentos;
+
+public class QuestionarioTemperamentos {
+
+    public static void main(String[] args) {
+        QuestionarioView tela = new QuestionarioView();
+        tela.setVisible(true);
+    }
+}
